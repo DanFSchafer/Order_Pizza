80d89ef3ff73f19b5d4f114b77dbecddfd359063
Dan Schafer <danfschafer@gmail.com>
2016-07-16 11:15:06 -0700

    tried to add ability to order multiple pizzas, couldn't quite get there.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// Order constructor
+
+function Order(pizzas, salads) {
+  this.pizzas = pizzas;
+  this.salads = salads;
+};
+
 // Pizza constructor
 function Pizza(name, toppings, size, finalCost) {
   this.name = name;
@@ -36,29 +43,50 @@ Pizza.prototype.cost = function() {
   return currentCost;
 };
 
+var pizza_index = 0
+
 // jQuery
 $(function() {
-//Display images of toppings
 
 
-//Pizza options submit button
+//Price this PIZZA submit button
   $("#options form").submit(function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-//get data from forms
-  var pizzaSize = $("input:radio[name=size]:checked").val();
-  var toppers = [];
-  $.each($("input:checkbox[name='toppings']:checked"), function() {
-    toppers.push(parseInt(($(this).val())));
+  //get data from forms
+    var pizzaSize = $("input:radio[name=size]:checked").val();
+    var toppers = [];
+    $.each($("input:checkbox[name='toppings']:checked"), function() {
+      toppers.push(parseInt(($(this).val())));
+    });
+
+  //create pizza object, and calculate cost
+    var newPizza = new Pizza("", toppers, pizzaSize, 0);
+    newPizza.finalCost = newPizza.cost();
+
+    var newOrder = new Order([newPizza]);
+    // newOrder.pizzas.push("foobar");
+    console.log(newOrder);
+
+  //display results
+    $("#results").show();
+    // $("#cost").text("");
+    $("#cost").text("$ " + newPizza.finalCost.toFixed(2));
   });
 
-//create pizza object, and calculate cost
-  var newPizza = new Pizza("", toppers, pizzaSize, 0);
-  newPizza.finalCost = newPizza.cost();
+//Add another PIZZA ?
+  $("#another_pizza form").submit(function(event) {
+    event.preventDefault();
+    alert("OK, you got it");
+    pizza_index +=1;
+    //clear form
+    $("#options form").checkboxes('uncheck');
+// need to clear the radio buttons
+    $("#num_pizzas").text("Your order consists of " + pizza_index + "pizza(s).")
 
-//display results
-  $("#results").show();
-  // $("#cost").text("");
-  $("#cost").text("$ " + newPizza.finalCost.toFixed(2));
+  });
+//Final Pizza Checkout
+  $("#final_pizza").click(function() {
+//code here
   });
 });
